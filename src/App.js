91fff5ec35824a7197aae305a5dc71c0221f0d2b
Nosov-Pavel <nopaev@gmail.com>
2021-08-19 15:29:37 -0400

    PenImg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import pen from './Images/pen.jpeg'
 import Menu from "./Menu";
 import Catalog from "./Catalog";
 
@@ -7,10 +7,12 @@ function App() {
 
     const menu = ['Catalog', 'Main page', 'Q&A', 'Reviews'];
     const catalogName = 'Catalog';
+    const cards = [{prodName: 'pen', img: pen, }]
 
   return (
     <div className="App">
      <h1>SHOP</h1>
+        <img src={pen}/>
         <Menu name={menu}/>
         <Catalog catalog={catalogName}/>
     </div>
