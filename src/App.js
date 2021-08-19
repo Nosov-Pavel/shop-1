8b8849aa7d711df0bab840845eed18942fb3ab52
Nosov-Pavel <nopaev@gmail.com>
2021-08-19 14:53:58 -0400

    catalog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
 import Menu from "./Menu";
+import Catalog from "./Catalog";
 
 function App() {
 
     const menu = ['Catalog', 'Main page', 'Q&A', 'Reviews'];
+    const catalogName = 'Catalog';
 
   return (
     <div className="App">
      <h1>SHOP</h1>
         <Menu name={menu}/>
+        <Catalog catalog={catalogName}/>
     </div>
   );
 }
---
+++ src/Catalog.js
@@ -0,0 +1,10 @@
+function Catalog(props){
+    return(
+        <div>
+            <h2>{props.catalog}</h2>
+
+        </div>
+
+    );
+}
+export default Catalog;
