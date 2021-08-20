f0599b292398e81415aad4e2b7a1cf80be0d22e2
Nosov-Pavel <nopaev@gmail.com>
2021-08-20 19:39:46 -0400

    button

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
 
     const menu = ['Catalog', 'Main page', 'Q&A', 'Reviews'];
     const catalogName = 'Catalog';
-    const cards = [{prodName: 'pen', img: pen, price: 3 },
+    const cards = [{prodName: 'sharpener', img: sharpener, price: 5 },
+        {prodName: 'pen', img: pen, price: 3 },
         {prodName: 'pencil', img: pencil, price: 2 },
         {prodName: 'sharpener', img: sharpener, price: 5 },
         {prodName: 'eraser', img: eraser, price: 1 } ]
--- src/Catalog.js
+++ src/Catalog.js
@@ -1,16 +1,17 @@
 import ProductCard from "./ProductCard";
 
-function Catalog(props){
-    return(
-       <div>
-           <h2>{props.name}</h2>
+function Catalog(props) {
+    return (
+        <div>
+            <h2>{props.name}</h2>
 
-       <div className='wrapper'>
-           {props.product.map(el =><ProductCard product={el} buyButtonLabel={props.buyButtonLabel}/>)}
-       </div>
-       </div>
+            <div className='wrapper'>
+                {props.product.map(el => <ProductCard product={el} buyButtonLabel={props.buyButtonLabel}/>)}
+            </div>
+        </div>
 
 
     );
 }
+
 export default Catalog;
