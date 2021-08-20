eaee314c07238ee47252ddf44ceeea6281106001
Nosov-Pavel <nopaev@gmail.com>
2021-08-20 12:33:46 -0400

    div

--- src/Catalog.js
+++ src/Catalog.js
@@ -2,11 +2,15 @@ import ProductCard from "./ProductCard";
 
 function Catalog(props){
     return(
-        <div className='wrapper'>
-            <h2>{props.catalog}</h2>
-            {props.product.map(el =><ProductCard product={el}/>)}
+       <div>
+           <h2>{props.catalog}</h2>
 
-        </div>
+       <div className='wrapper'>
+
+            {props.product.map(el =><ProductCard product={el}/>)}
+       </div>
+       </div>
+
 
     );
 }
