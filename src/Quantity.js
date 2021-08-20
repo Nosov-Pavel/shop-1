d64c43d4439facb35bc3d762753050a22fe99d3a
Nosov-Pavel <nopaev@gmail.com>
2021-08-20 12:41:01 -0400

    quantity

---
+++ src/Quantity.js
@@ -0,0 +1,12 @@
+function Quantity(props){
+    return(
+        <div>
+            <button>-</button>
+            <p>1</p>
+            <button>+</button>
+            <button>Buy</button>
+        </div>
+
+    );
+}
+export default Quantity;
